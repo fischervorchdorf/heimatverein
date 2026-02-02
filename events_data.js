737403e32fcf401d34c6e1f663f7fdd90a53375d
fischervorchdorf <fischervorchdorf@gmx.at>
2026-02-02 15:53:55 +0100

    events erweitert

--- events_data.js
+++ events_data.js
@@ -3,31 +3,38 @@
 
 const eventsData = [
     {
-        title: "Frühjahrsputz im Museum Vorchdorf",
-        date: "2026-05-10",
-        time: "09:00 Uhr",
-        description: "Gemeinsamer Einsatz zur Verschönerung unseres Museums und der Umgebung.",
-        location: "Museum Vorchdorf"
+        "title": "Frühjahrsputz im Museum Vorchdorf",
+        "date": "2026-05-10",
+        "time": "09:00 Uhr",
+        "description": "Gemeinsamer Einsatz zur Verschönerung unseres Museums und der Umgebung.",
+        "location": "Museum Vorchdorf"
     },
     {
-        title: "Gemeinsame Fahrt zur Burg Wels",
-        date: "2026-04-20",
-        time: "14:00 Uhr",
-        description: "Geführter Rundgang durch historische Orte unserer Nachbarstadt. Treffpunkt: Kirche Vorchdorf",
-        location: "Kirche Vorchdorf"
+        "title": "Gemeinsame Fahrt zur Burg Wels",
+        "date": "2026-04-20",
+        "time": "14:00 Uhr",
+        "description": "Geführter Rundgang durch historische Orte unserer Nachbarstadt. Treffpunkt: Kirche Vorchdorf",
+        "location": "Kirche Vorchdorf"
     },
     {
-        title: "Heimatvereinstreffen zur Fertigstellung des Bücherregals",
-        date: "2025-11-17",
-        time: "19:00 Uhr",
-        description: "Gemeinsames Treffen zur Fertigstellung des Bücherregals.",
-        location: "Museum Vorchdorf"
+        "title": "Vorabinfo für unseren neuen Sonderausstellung \"Museum meets KI\"",
+        "date": "2026-03-26",
+        "time": "19:00 Uhr",
+        "description": "Wir versuchen mit dieser Ausstellung allen Interessierten zu zeigen, was KI ist und kann und vor allem was KI \"noch nicht\" kann. Zusätzlich haben wir eigene KI-Apps geschrieben, die alle möglichen Artefakte durchchecken und nach unseren \"Prompts\" eine umfassende Information ausspucken. Lassen Sie sich überraschen und probieren Sie es selbst aus - sie werden staunen!",
+        "location": "Museum der Region Vorchdorf"
     },
     {
-        title: "Generalversammlung 2025",
-        date: "2025-11-14",
-        time: "19:00 Uhr",
-        description: "Ordentliche Generalversammlung des Heimat- und Kulturvereins. Wir freuen uns auf zahlreiches Erscheinen!",
-        location: "Gasthaus Schloss Hochhaus"
+        "title": "Heimatvereinstreffen zur Fertigstellung des Bücherregals",
+        "date": "2025-11-17",
+        "time": "19:00 Uhr",
+        "description": "Gemeinsames Treffen zur Fertigstellung des Bücherregals.",
+        "location": "Museum Vorchdorf"
+    },
+    {
+        "title": "Generalversammlung 2025",
+        "date": "2025-11-14",
+        "time": "19:00 Uhr",
+        "description": "Ordentliche Generalversammlung des Heimat- und Kulturvereins. Wir freuen uns auf zahlreiches Erscheinen!",
+        "location": "Gasthaus Schloss Hochhaus"
     }
 ];
